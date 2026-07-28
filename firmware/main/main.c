17be425698b8a7f3f9bf0f699e8f6e73043eaaa8
cole <atolase120@gmail.com>
2026-07-28 11:51:48 +0100

    updated doc

---
+++ firmware/main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LOW_PRIORITY     3
+#define MID_PRIORITY     6
+#define HIGH_PRIORITY    9
+
+const char* TAG = "EVSE";
+
+void app_main(void)
+{
+
+}
